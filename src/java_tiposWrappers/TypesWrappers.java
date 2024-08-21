e7b72360eec3cfe6dd89a7b89b04de490b749d6f
SuelenGalhardo <suelenranucci@gmail.com>
2024-08-21 13:42:20 +0200

    Add types wrappers

---
+++ src/java_tiposWrappers/TypesWrappers.java
@@ -0,0 +1,32 @@
+package java_tiposWrappers;
+
+public class TypesWrappers {
+    public static void main(String[] args) {
+
+     /**   double numberDouble = 100.0;
+     int numberInt = (int) numberDouble;
+     System.out.println(numberInt);  // Output: 100
+
+     Double d = 100.0;
+     Integer i = d.intValue()   ; //
+     System.out.println(i);  // Output: 100
+
+     double d = 3.75;
+     int i = (int) d;
+     System.out.println(i);  // Output: 3
+
+     String num = "100";
+     int i = Integer.parseInt(num);
+     System.out.println(i);  // Output: 100
+
+        String num = "100.123";
+        double i = Double.parseDouble(num);
+        System.out.println(i);  // Output: 100
+
+      Double valor = 123.456;
+      valor.**/
+
+
+    }
+}
+
